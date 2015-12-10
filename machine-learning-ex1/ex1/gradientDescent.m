45c4d21caea1a83aa4afa2a38d101591982d0ee1
koallen <weifengzi2009@gmail.com>
2015-12-10 20:12:07 +0800

    finish ex1 compulsory part

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,15 +17,11 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    theta = theta - alpha / m * (X.' * ((X * theta) - y))
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCost(X, y, theta);
 
 end
