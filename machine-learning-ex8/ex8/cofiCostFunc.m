9bca9be23055b6a2668a81ddbe786c2ffb37b264
koallen <weifengzi2009@gmail.com>
2015-12-17 22:24:40 +0800

    finish ex8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -11,7 +11,7 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
+
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -40,20 +40,12 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = sum(sum(((X * Theta.') - Y) .^ 2 .* R)) / 2 + lambda / 2 * sum(sum(Theta .^ 2)) ...
+    + lambda / 2 * sum(sum(X .^ 2));
 
+X_grad = (X * Theta.' - Y) .* R * Theta + lambda * X;
 
-
-
-
-
-
-
-
-
-
-
-
-
+Theta_grad = ((X * Theta.' - Y) .* R).' * X + lambda * Theta;
 
 % =============================================================
 
